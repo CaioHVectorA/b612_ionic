bb82da0b2758799679ef7fb9d334c178979feaaa
Caio Henrique Oliveira Batista <caihebatista@gmail.com>
2023-08-27 01:49:52 -0300

    conection to api on board and some fixes in integration

--- src/components/pagesComponents/Avisos.tsx
+++ src/components/pagesComponents/Avisos.tsx
@@ -1,29 +1,30 @@
+import { useEffect, useState } from "react";
 import { ColumnContainer } from "../styled/container";
+import { URL } from "../../utils/envariables";
 
-type Author = {
-  name: string;
-  img: string;
-};
+
 type AvisoProps = {
-  author: Author;
+  author: string; 
+  img: string;
   title: string;
   body: string;
+  tags: string;
 };
 
-function Aviso({ author, body, title, index }: AvisoProps & { index: number }) {
+function Aviso({ author, body, title, index, img, tags, length }: AvisoProps & { index: number, length: number }) {
   return (
     <ColumnContainer
       className=" flex-1 gap-2 py-3 w-full"
-      style={{ height: "100%", marginLeft: index === 0 ? "16px" : "0px" }}
+      style={{ height: "100%", marginLeft: index === length - 1 ? "16px" : "0px", marginRight: index === 0 ? '16px' : '0px' }}
     >
       <div className=" rounded-3xl bg-main h-1/5 w-72 flex gap-2 px-2 items-center py-4">
         <img
-          src={author.img}
-          className=" rounded-full max-w-full"
+          src={img}
+          className=" rounded-full max-w-full aspect-square object-center object-cover" // IMAGEM SERÁ QUADRADOS
           style={{ width: "12vh" }}
         />
         <h1 className="uppercase text-3xl align-baseline text-bg">
-          {author.name}
+          {author}
         </h1>
       </div>
       <div className=" rounded-3xl bg-light shadow-xl px-3 py-2 h-4/5 w-72">
@@ -36,11 +37,18 @@ function Aviso({ author, body, title, index }: AvisoProps & { index: number }) {
   );
 }
 
-export default function Avisos({ avisos }: { avisos: AvisoProps[] }) {
+export default function Avisos() {
+  const [avisos, setAvisos] = useState<AvisoProps[]>([])
+  useEffect(() => {
+    fetch(URL+'/aviso/').then(res => res.json()).then(data => setAvisos(data))
+  }, [])
   return (
-    <div className=" flex overflow-x-auto scroll-smooth gap-3 snap-x snap-mandatory h-4/5">
+    <div className=" flex flex-row-reverse overflow-x-auto scroll-smooth gap-3 snap-x snap-mandatory h-4/5">
       {avisos.map((item, index) => (
         <Aviso
+          tags={item.tags}
+          img={item.img}
+          length={avisos.length}
           title={item.title}
           index={index}
           author={item.author}
