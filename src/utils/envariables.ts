64681d72e02ee74a074195fac1e089d4563d200c
Caio Henrique <109510970+CaioHVectorA@users.noreply.github.com>
2023-09-06 20:15:39 +0000

    Update 3 files

--- src/utils/envariables.ts
+++ src/utils/envariables.ts
@@ -1,3 +1,3 @@
-export const URL = 'http://localhost:9128'
-// export const URL = 'https://3v7gld-9128.csb.app'
+//export const URL = 'http://localhost:9128'
+export const URL = "https://3v7gld-9128.csb.app";
 // export const TURMA = 3001
